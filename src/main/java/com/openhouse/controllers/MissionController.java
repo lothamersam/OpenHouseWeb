8abd5642dd9a5a41bc705f9e86dca3ce44e0a21a
Sam Lothamer <lothamer.sam@gmail.com>
2019-06-09 18:00:46 -0400

    add dropdown for navbar

--- src/main/java/com/openhouse/controllers/MissionController.java
+++ src/main/java/com/openhouse/controllers/MissionController.java
@@ -12,7 +12,7 @@ public class MissionController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/WEB-INF/pages/missions.jsp").forward(request, response);
+		request.getRequestDispatcher("/WEB-INF/pages/mission.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- src/main/webapp/WEB-INF/tags/meta.tag
+++ src/main/webapp/WEB-INF/tags/meta.tag
@@ -3,5 +3,9 @@
 <head>
 	<title>${title}: Open House</title>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.css" />
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+	<script language="javascript" src="../static/meta.js"></script>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
