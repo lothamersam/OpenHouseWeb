aebbd48bf4cf165a87a82370029f65c9b6c35b26
Sam Lothamer <lothamer.sam@gmail.com>
2019-06-09 17:09:15 -0400

    added basic site controllers

---
+++ src/main/java/com/openhouse/controllers/ContactController.java
@@ -0,0 +1,21 @@
+package com.openhouse.controllers;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/contact")
+public class ContactController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/pages/contact.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
