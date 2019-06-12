8ebe674859111195ab428847244e36ea5de8b8b4
Sam Lothamer <lothamer.sam@gmail.com>
2019-06-11 23:09:50 -0400

    add email service and dependency for sendgrid

---
+++ src/main/java/com/openhouse/controllers/actions/DoEmailController.java
@@ -0,0 +1,51 @@
+
+package com.openhouse.controllers.actions;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sendgrid.Email;
+import com.sendgrid.Content;
+import com.sendgrid.Mail;
+import com.sendgrid.Request;
+import com.sendgrid.SendGrid;
+import com.sendgrid.Response;
+import com.sendgrid.Method;
+
+@WebServlet("/action/email")
+public class DoEmailController extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Email from = new Email("test@example.com");
+        String subject = "Hello World from the SendGrid Java Library!";
+        Email to = new Email("lothamer.sam@gmail.com");
+        Content content = new Content("text/plain", "Hello, Email!");
+        Mail mail = new Mail(from, subject, to, content);
+
+        SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+        Request emailRequest = new Request();
+        try {
+            emailRequest.method = Method.POST;
+            emailRequest.endpoint = "mail/send";
+            emailRequest.body = mail.build();
+            Response emailResponse = sg.api(emailRequest);
+            System.out.println(emailResponse.statusCode);
+            System.out.println(emailResponse.body);
+            System.out.println(emailResponse.headers);
+        } catch (IOException ex) {
+            throw ex;
+        }
+
+    }
+}
