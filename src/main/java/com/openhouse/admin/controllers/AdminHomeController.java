7b820e44138131d89860b6991cf446cea5b8879a
Sam Lothamer <lothamer.sam@gmail.com>
2019-06-30 17:05:28 -0400

    add admin pages start staff page edit

---
+++ src/main/java/com/openhouse/admin/controllers/AdminHomeController.java
@@ -0,0 +1,18 @@
+package com.openhouse.admin.controllers;
+
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/admin")
+public class AdminHomeController extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+    }
+}
