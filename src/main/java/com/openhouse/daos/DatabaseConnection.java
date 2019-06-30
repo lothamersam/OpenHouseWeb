8e680ee51af49b6fc17be3fc7a5e292ba3efbe5f
Sam Lothamer <lothamer.sam@gmail.com>
2019-06-29 23:03:27 -0400

    change connection wrapper

--- src/main/java/com/openhouse/daos/DatabaseConnection.java
+++ src/main/java/com/openhouse/daos/DatabaseConnection.java
@@ -7,20 +7,14 @@ import java.sql.DriverManager;
 import java.net.URI;
 
 public class DatabaseConnection {
-    private static Connection connection;
-
     public static Connection getConnection() throws URISyntaxException, SQLException {
-        if (connection == null) {
-            URI dbUri = new URI(System.getenv("DATABASE_URL"));
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
-            String username = dbUri.getUserInfo().split(":")[0];
-            String password = dbUri.getUserInfo().split(":")[1];
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
 
-            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
 
-            connection = DriverManager.getConnection(dbUrl, username, password);
-        }
-
-        return connection;
+        return DriverManager.getConnection(dbUrl, username, password);
     }
 }
