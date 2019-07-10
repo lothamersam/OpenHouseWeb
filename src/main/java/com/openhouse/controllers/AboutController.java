00773d6d0c0c739719aecfa5a811244208a1f1dc
Sam Lothamer <lothamer.sam@gmail.com>
2019-07-09 21:45:31 -0400

    placholder attribute

--- src/main/java/com/openhouse/controllers/AboutController.java
+++ src/main/java/com/openhouse/controllers/AboutController.java
@@ -12,6 +12,8 @@ public class AboutController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("aboutUsSnippet", "<p>This is temorary placeholder text</p>");
+		
 		request.getRequestDispatcher("/WEB-INF/pages/about.jsp").forward(request, response);
 	}
 }
