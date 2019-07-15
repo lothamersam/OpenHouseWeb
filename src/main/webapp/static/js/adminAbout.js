76351fdcad5a6d4dcfdbeb07d8cc4f24e0056a1f
Sam Lothamer <lothamer.sam@gmail.com>
2019-07-14 23:09:32 -0400

    testing

--- src/main/webapp/static/js/adminAbout.js
+++ src/main/webapp/static/js/adminAbout.js
@@ -1,6 +1,7 @@
 $("div").click(function() {
 	$("#name").val($(this).attr("id"))
+	console.log($(this).attr("id"))
 	$("#sectionContentInput").html($(this).find("#sectionContent:first").html())
 	
-	$("#editModal").show()
+	$("#editModal").open('open')
 });
