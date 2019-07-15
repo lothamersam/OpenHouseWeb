f6c35ec3b322fd70f686caf334d20d52fa2d6958
Sam Lothamer <lothamer.sam@gmail.com>
2019-07-14 22:09:58 -0400

    added missing mapping

--- src/main/java/com/openhouse/admin/controllers/EditAboutController.java
+++ src/main/java/com/openhouse/admin/controllers/EditAboutController.java
@@ -3,6 +3,7 @@ package com.openhouse.admin.controllers;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,6 +12,7 @@ import com.openhouse.daos.AboutDao;
 import com.openhouse.factory.DaoFactory;
 import com.openhouse.services.enums.AboutSectionType;
 
+@WebServlet("/admin/about")
 public class EditAboutController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private final AboutDao aboutDao = DaoFactory.getAboutDao();
