de5d7b826dd2993b12dd048a6767a540d884bee5
Sam Lothamer <lothamer.sam@gmail.com>
2019-07-14 23:31:53 -0400

    fix label bug

--- src/main/webapp/static/js/adminAbout.js
+++ src/main/webapp/static/js/adminAbout.js
@@ -1,5 +1,6 @@
 $("div.tooltipped").click(function() {
 	$("#title").val($(this).attr("id"))
+	$("#title").next().addClass("active")
 	$(".trumbowyg-editor").html($(this).find("#sectionContent:first").html())
 	
 	$("#editModal").modal('open')
