09cf80958244de824aaff9b5838a29e6a81b8cb1
Sam Lothamer <lothamer.sam@gmail.com>
2019-07-14 23:12:13 -0400

    add log lines for testing

--- src/main/webapp/static/js/adminAbout.js
+++ src/main/webapp/static/js/adminAbout.js
@@ -1,6 +1,7 @@
 $("div").click(function() {
 	$("#name").val($(this).attr("id"))
 	console.log($(this).attr("id"))
+	console.log($(this).find("#sectionContent:first").html())
 	$("#sectionContentInput").html($(this).find("#sectionContent:first").html())
 	
 	$("#editModal").modal('open')
