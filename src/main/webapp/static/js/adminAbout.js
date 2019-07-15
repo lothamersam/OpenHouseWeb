f758be9c18e687bd75c0375a6b26244ed6c4cb9c
Sam Lothamer <lothamer.sam@gmail.com>
2019-07-14 23:06:48 -0400

    add missing quotes

--- src/main/webapp/static/js/adminAbout.js
+++ src/main/webapp/static/js/adminAbout.js
@@ -1,5 +1,5 @@
 $("div").click(function() {
-	$("#name").val($(this).attr(id))
+	$("#name").val($(this).attr("id"))
 	$("#sectionContentInput").html($(this).find("#sectionContent:first").html())
 	
 	$("#editModal").show()
