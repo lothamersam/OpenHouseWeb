cc4dd206680f399962efd7248044724d8786ea4f
Sam Lothamer <lothamer.sam@gmail.com>
2019-07-14 23:18:08 -0400

    section set text area content

--- src/main/webapp/static/js/adminAbout.js
+++ src/main/webapp/static/js/adminAbout.js
@@ -1,7 +1,6 @@
 $("div.tooltipped").click(function() {
 	$("#title").val($(this).attr("id"))
-	console.log($(this).find("#sectionContent:first").html())
-	$("#sectionContentInput").html($(this).find("#sectionContent").html())
+	$("#sectionContentInput").html($(this).find("#sectionContent:first").html())
 	
 	$("#editModal").modal('open')
 });
