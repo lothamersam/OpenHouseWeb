b02a3934b251d14635344e49d38c32acf9852694
Sam Lothamer <36243786+lothamersam@users.noreply.github.com>
2019-07-23 15:48:02 -0400

    Create PageSectionChangeREST.java

---
+++ src/main/java/com/openhouse/rest/admin/PageSectionChangeREST.java
@@ -0,0 +1,30 @@
+package com.openhouse.admin.controllers.actions;
+
+import javax.ws.rs.GET;  
+import javax.ws.rs.Path;  
+import javax.ws.rs.PathParam;  
+import javax.ws.rs.core.Response; 
+
+import com.openhouse.beans.PageSectionTO;
+import com.openhouse.daos.BasicPageDao;
+import com.openhouse.factory.DaoFactory;
+import com.openhouse.factory.ServiceFactory;
+import com.openhouse.services.ParameterService;
+
+@Path("/admin/page")
+public class PageSectionChangeREST {	
+	  private final ParameterService parameterService = ServiceFactory.getParameterService();
+    private final BasicPageDao aboutDao = DaoFactory.getPageDao();
+    
+    @POST
+    @Path("/change")
+    public Response changeHomeSection() throws ServletException, IOException {
+    	PageSectionTO aboutSection = this.parameterService.getPageSectionFromRequest(request);
+
+        boolean status = this.aboutDao.editPageSection(aboutSection);
+
+        return Response.status(200)  
+          .entity("test")  
+          .build();  
+    }
+}
