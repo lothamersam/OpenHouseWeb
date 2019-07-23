b9e922664e8dd5aef3c58c67dc079ed0576ba2df
Sam Lothamer <lothamer.sam@gmail.com>
2019-07-22 20:59:26 -0400

    fix error

--- src/main/webapp/static/js/standardPage.js
+++ src/main/webapp/static/js/standardPage.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
     $('.timepicker').pickatime({
     	default: 'now', // Set default time: 'now', '1:30AM', '16:30'
     	fromnow: 0,
-    	twelvehour: TRUE, // Use AM/PM or 24-hour format
+    	twelvehour: true, // Use AM/PM or 24-hour format
     	donetext: 'OK', // text for done-button
     	cleartext: 'Clear', // text for clear-button
     	canceltext: 'Cancel', // Text for cancel-button,
