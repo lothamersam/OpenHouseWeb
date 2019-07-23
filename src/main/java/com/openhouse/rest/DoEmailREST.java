e57bbdc8115c2aec3b8ec50e17be417e1597b96c
Sam Lothamer <36243786+lothamersam@users.noreply.github.com>
2019-07-23 15:31:54 -0400

    Update DoEmailREST.java

--- src/main/java/com/openhouse/rest/DoEmailREST.java
+++ src/main/java/com/openhouse/rest/DoEmailREST.java
@@ -9,7 +9,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.FormParam;  
 import javax.ws.rs.core.Response; 
 
-
 @Path("/email")
 public class DoEmailREST {  
     private static final EmailService emailService = ServiceFactory.getEmailService();
