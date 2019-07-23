d86142f5ba0a2f1ebf66807deb6b7523658f1e27
Sam Lothamer <36243786+lothamersam@users.noreply.github.com>
2019-07-23 14:18:09 -0400

    Update DoGetTimeSlotsController.java

--- src/main/java/com/openhouse/controllers/actions/DoGetTimeSlotsController.java
+++ src/main/java/com/openhouse/controllers/actions/DoGetTimeSlotsController.java
@@ -5,15 +5,13 @@ import com.openhouse.services.ParameterService;
 import com.openhouse.factory.ServiceFactory;
 import com.openhouse.factory.DaoFactory;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.GET;  
+import javax.ws.rs.Path;  
+import javax.ws.rs.PathParam;  
+import javax.ws.rs.core.Response; 
 
 @Path("/times")
-public class DoGetTimeSlotsController extends HttpServlet {
+public class DoGetTimeSlotsController {
     private static final long serialVersionUID = 1L;
     
     private static final ParameterService parameterService = ServiceFactory.getParameterService();
