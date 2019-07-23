48a25656cffd27c424bcadcaa3732c6d7d1ea5c9
Sam Lothamer <36243786+lothamersam@users.noreply.github.com>
2019-07-23 15:30:10 -0400

    Create DoEmailREST.java

---
+++ src/main/java/com/openhouse/rest/DoEmailREST.java
@@ -0,0 +1,28 @@
+package com.openhouse.rest;
+
+import com.openhouse.services.EmailService;
+import com.openhouse.services.ParameterService;
+import com.openhouse.factory.ServiceFactory;
+
+import javax.ws.rs.POST;  
+import javax.ws.rs.Path;  
+import javax.ws.rs.FormParam;  
+import javax.ws.rs.core.Response; 
+
+
+@Path("/email")
+public class DoEmailController extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    
+    private static final EmailService emailService = ServiceFactory.getEmailService();
+    private static final ParameterService parameterService = ServiceFactory.getParameterService();
+
+    @POST
+    @Path("/send/")
+    protected Response doSendEmail(@FormParam("firstName") String firstName) {
+      
+      return Response.status(200)  
+        .entity(name)  
+        .build(); 
+    }
+}
