2d38f6f92109d1fabb69b88d570a53f16c1852ea
Sam Lothamer <36243786+lothamersam@users.noreply.github.com>
2019-07-23 15:30:58 -0400

    Update DoEmailREST.java

--- src/main/java/com/openhouse/rest/DoEmailREST.java
+++ src/main/java/com/openhouse/rest/DoEmailREST.java
@@ -11,7 +11,7 @@ import javax.ws.rs.core.Response;
 
 
 @Path("/email")
-public class DoEmailController extends HttpServlet {
+public class DoEmailREST extends HttpServlet {
     private static final long serialVersionUID = 1L;
     
     private static final EmailService emailService = ServiceFactory.getEmailService();
