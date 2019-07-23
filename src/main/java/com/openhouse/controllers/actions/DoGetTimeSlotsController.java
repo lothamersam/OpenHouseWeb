94fc72c768523f108cb5737033c050b1e4218dad
Sam Lothamer <36243786+lothamersam@users.noreply.github.com>
2019-07-23 13:45:58 -0400

    Create DoGetTimeSlotsController.java

---
+++ src/main/java/com/openhouse/controllers/actions/DoGetTimeSlotsController.java
@@ -0,0 +1,27 @@
+package com.openhouse.controllers.actions;
+
+import com.openhouse.services.ParameterService;
+import com.openhouse.factory.ServiceFactory;
+import com.openhouse.factory.DaoFactory;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/action/email")
+public class DoGetTimeSlotsController extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    
+    private static final ParameterService parameterService = ServiceFactory.getParameterService();
+    private static final AuditionDao auditionDao = DaoFactory.getAuditionDao();
+
+    //TODO: Change return JSON
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+            response.getWriter().write("true");
+    }
+}
