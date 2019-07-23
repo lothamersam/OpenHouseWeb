dabad5df324fc19aa0306c4051a094b1073cc397
Sam Lothamer <36243786+lothamersam@users.noreply.github.com>
2019-07-23 15:49:56 -0400

    Update PageSectionChangeREST.java

--- src/main/java/com/openhouse/rest/admin/PageSectionChangeREST.java
+++ src/main/java/com/openhouse/rest/admin/PageSectionChangeREST.java
@@ -1,6 +1,6 @@
 package com.openhouse.rest.admin;
 
-import javax.ws.rs.GET;  
+import javax.ws.rs.POST;  
 import javax.ws.rs.Path;  
 import javax.ws.rs.PathParam;  
 import javax.ws.rs.core.Response; 
