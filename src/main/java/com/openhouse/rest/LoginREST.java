5bb90d5ae3fe5af2fda3da2e8772e282286219ec
Sam Lothamer <36243786+lothamersam@users.noreply.github.com>
2019-07-24 15:27:29 -0400

    Create LoginREST.java

---
+++ src/main/java/com/openhouse/rest/LoginREST.java
@@ -0,0 +1,28 @@
+package com.openhouse.rest;
+
+import javax.servlet.http.HttpServletRequest;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+@Path("/login")
+public class PageSectionChangeREST {
+
+	@POST
+	public Response verifyUserAndLogin(@Context UriInfo uriInfo, 
+      @Context HttpServletRequest request,
+			@FormParam("username") String username,
+			@FormParam("password") String password) {
+
+    System.out.println(username + " " + password);
+    
+    request.setAttribute("user", "test");
+
+		return Response.seeOther(uriInfo.getBaseUriBuilder().path("../admin/").build()).build();
+	}
+}
