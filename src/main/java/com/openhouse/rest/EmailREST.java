9b121c23e2d329e40ada0368aee378765c974f4f
Sam Lothamer <lothamer.sam@gmail.com>
2019-07-24 00:23:27 -0400

    add produces

--- src/main/java/com/openhouse/rest/EmailREST.java
+++ src/main/java/com/openhouse/rest/EmailREST.java
@@ -6,7 +6,9 @@ import com.openhouse.factory.ServiceFactory;
 
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.FormParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.json.JSONObject;
@@ -20,6 +22,7 @@ public class EmailREST {
 
 	@POST
 	@Path("/send")
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response doSendEmail(@FormParam("firstName") String firstName, 
 			@FormParam("lastName") String lastName,
 			@FormParam("email") String email, 
