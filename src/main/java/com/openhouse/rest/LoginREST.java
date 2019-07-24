f8d57be29ba14ab3bc1f3fc68fa0f81e02609baf
Sam Lothamer <lothamer.sam@gmail.com>
2019-07-24 19:32:37 -0400

    revert file for now

--- src/main/java/com/openhouse/rest/LoginREST.java
+++ src/main/java/com/openhouse/rest/LoginREST.java
@@ -9,37 +9,19 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
-import org.apache.commons.lang3.StringUtils;
-import org.mindrot.jbcrypt.BCrypt;
-
-import com.openhouse.beans.UserTO;
-import com.openhouse.daos.UserDao;
-import com.openhouse.factory.DaoFactory;
-
 @Path("/login")
 public class LoginREST {
-	private final UserDao userDao = DaoFactory.getUserDao();
 
-	
 	@POST
 	public Response verifyUserAndLogin(@Context UriInfo uriInfo,
 			@Context HttpServletRequest request,
 			@FormParam("username") String username, 
 			@FormParam("password") String password) {
-		boolean success = false;
-		String redirectPath = "../login?error=Invalid username/password combination!";
-		final UserTO user = this.userDao.getUser(username);
-		
-		if(StringUtils.isNotBlank(user.getUsername())) {
-			if(StringUtils.isNotBlank(user.getPassword())) {
-				success = BCrypt.checkpw(password, BCrypt.gensalt(10));
-			}
-		}
-		
-		if(success) {
-			redirectPath = "../admin";
-		}
 
-		return Response.seeOther(uriInfo.getBaseUriBuilder().path(redirectPath).build()).build();
+		System.out.println(username + " " + password);
+
+		request.getSession().setAttribute("user", "test");
+
+		return Response.seeOther(uriInfo.getBaseUriBuilder().path("../admin/").build()).build();
 	}
 }
