b561f7f406450101b68b03929f65a16efa8b631e
Sam Lothamer <lothamer.sam@gmail.com>
2019-07-24 20:38:36 -0400

    add user verification

--- src/main/java/com/openhouse/daos/UserDao.java
+++ src/main/java/com/openhouse/daos/UserDao.java
@@ -9,13 +9,14 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.json.JSONObject;
 
 import com.openhouse.beans.UserTO;
 
-
 public class UserDao {
 	private static final String GET_USER = "SELECT username, password, id FROM oh_uman WHERE username = ?";
+	private static final String GET_USER_ID = "SELECT username, password, id FROM oh_uman WHERE id = ?";
 	private static final String GET_USER_LIST = "SELECT id, first_name, last_name, username FROM oh_uman";
 	private static final String ADD_USER = "INSERT INTO oh_uman (first_name, last_name, username, password) VALUES (?, ?, ?, ?)";
 	private static final String REMOVE_USER = "DELETE FROM oh_uman WHERE id = ?";
@@ -28,43 +29,41 @@ public class UserDao {
 			final PreparedStatement statement = connection.prepareStatement(GET_USER);
 
 			statement.setString(1, username);
-			
+
 			ResultSet results = statement.executeQuery();
-			while(results.next()){
+			while (results.next()) {
 				user.setUsername(results.getString(1));
 				user.setPassword(results.getString(2));
 				user.setId(results.getInt(3));
 			}
 		} catch (URISyntaxException | SQLException e) {
 			System.out.println("There was an error when querying the database! " + e.getMessage());
-		} 
+		}
 
 		return user;
 	}
-	
+
 	public List<UserTO> getUserList() {
 		List<UserTO> users = new ArrayList<>();
 
 		try (final Connection connection = DatabaseConnection.getConnection()) {
 			final Statement statement = connection.createStatement();
 			final ResultSet results = statement.executeQuery(GET_USER_LIST);
-			
-			while(results.next()){
-				users.add(new UserTO(results.getInt(1), 
-						results.getString(2), 
-						results.getString(3), 
+
+			while (results.next()) {
+				users.add(new UserTO(results.getInt(1), results.getString(2), results.getString(3),
 						results.getString(4)));
 			}
 		} catch (URISyntaxException | SQLException e) {
 			System.out.println("There was an error when querying the database! " + e.getMessage());
-		} 
+		}
 
 		return users;
 	}
-	
+
 	public JSONObject addUser(UserTO user) {
 		final JSONObject response = new JSONObject();
-		
+
 		try (final Connection connection = DatabaseConnection.getConnection()) {
 			final PreparedStatement statement = connection.prepareStatement(ADD_USER);
 
@@ -78,42 +77,46 @@ public class UserDao {
 			}
 		} catch (URISyntaxException | SQLException e) {
 			System.out.println("There was an error when querying the database! " + e.getMessage());
-			
-			if(e instanceof SQLException) {
-				if(((SQLException) e).getErrorCode() == 1062) {
+
+			if (e instanceof SQLException) {
+				if (((SQLException) e).getErrorCode() == 1062) {
 					response.put("message", "Username already exists!");
 				} else {
 					response.put("message", "Could not add user!");
 				}
 			}
-		} 
-		
+		}
+
 		return response;
 	}
-	
+
 	public JSONObject updateUser(UserTO user) {
 		final JSONObject response = new JSONObject();
-		
-		try (final Connection connection = DatabaseConnection.getConnection()) {
-			final PreparedStatement statement = connection.prepareStatement(UPDATE_USER);
 
-			statement.setString(1, user.getPassword());
-			statement.setInt(2, user.getId());
+		if (this.verifyUser(user)) {
+			try (final Connection connection = DatabaseConnection.getConnection()) {
+				final PreparedStatement statement = connection.prepareStatement(UPDATE_USER);
 
-			if (statement.executeUpdate() > 0) {
-				response.put("message", "Password changed successfully!");
+				statement.setString(1, user.getPassword());
+				statement.setInt(2, user.getId());
+
+				if (statement.executeUpdate() > 0) {
+					response.put("message", "Password changed successfully!");
+				}
+			} catch (URISyntaxException | SQLException e) {
+				System.out.println("There was an error when querying the database! " + e.getMessage());
+				response.put("message", "Could not update your password!");
 			}
-		} catch (URISyntaxException | SQLException e) {
-			System.out.println("There was an error when querying the database! " + e.getMessage());
-			response.put("message", "Could not update your password!");
-		} 
-		
+		} else {
+			response.put("message", "Old password incorrect!");
+		}
+
 		return response;
 	}
-	
+
 	public JSONObject removeUser(int id) {
 		final JSONObject response = new JSONObject();
-		
+
 		try (final Connection connection = DatabaseConnection.getConnection()) {
 			final PreparedStatement statement = connection.prepareStatement(REMOVE_USER);
 
@@ -125,8 +128,29 @@ public class UserDao {
 		} catch (URISyntaxException | SQLException e) {
 			System.out.println("There was an error when querying the database! " + e.getMessage());
 			response.put("message", "Could not remove user!");
-		} 
-		
+		}
+
 		return response;
+	}
+
+	private boolean verifyUser(UserTO user) {
+		UserTO fetchedUser = new UserTO();
+
+		try (final Connection connection = DatabaseConnection.getConnection()) {
+			final PreparedStatement statement = connection.prepareStatement(GET_USER_ID);
+
+			statement.setInt(1, user.getId());
+
+			ResultSet results = statement.executeQuery();
+			while (results.next()) {
+				user.setUsername(results.getString(1));
+				user.setPassword(results.getString(2));
+				user.setId(results.getInt(3));
+			}
+		} catch (URISyntaxException | SQLException e) {
+			System.out.println("There was an error when querying the database! " + e.getMessage());
+		}
+
+		return StringUtils.equals(user.getPassword(), fetchedUser.getPassword());
 	}
 }
--- src/main/webapp/static/js/adminUsers.js
+++ src/main/webapp/static/js/adminUsers.js
@@ -1,13 +1,19 @@
-$("#passChage").submit(function (event) {
+$("#passChange").submit(function (event) {
     let passChangeData = $("#passChange").serialize();
 
-    $.post("/do/admin/users/change", passChangeData, function (response) {
-        Materialize.toast(respose["message"]);
-        $("#formReset").click();
-    })
-    .fail(function (response) {
-        Materialize.toast("There an error sending your email, please try again!", 4000);
-    });
+    if(checkFields()) {    
+    	$.post("/do/admin/users/change", passChangeData, function (response) {
+    		Materialize.toast(respose["message"]);
+    		$("#formReset").click();
+    	})
+    	.fail(function (response) {
+    		Materialize.toast("There an error sending your email, please try again!", 4000);
+    	});
+    }
 
     return false;
 })
+
+function checkFields() {
+	return true;
+}
