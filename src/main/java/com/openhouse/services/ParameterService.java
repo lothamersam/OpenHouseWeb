640aa4ffa5f2b7bd81dcd2b4a87059ee0e3c0ab7
Sam Lothamer <lothamer.sam@gmail.com>
2019-08-12 18:55:37 -0400

    admin productions page

--- src/main/java/com/openhouse/services/ParameterService.java
+++ src/main/java/com/openhouse/services/ParameterService.java
@@ -29,181 +29,153 @@ import com.openhouse.services.enums.PageSectionType;
 import com.sendgrid.Content;
 
 public final class ParameterService {
-    private static final String CONTACT_DIRECTOR = "Contact Director";
+	private static final String CONTACT_DIRECTOR = "Contact Director";
 
-    public Mail getContactMailFromRequest(String firstName, 
-					  String lastName,
-					  String email,
-					  String phoneNumber,
-					  String category,
-					  String message) throws IllegalFormatException {
-        final Mail mail = new Mail();
-        
-        final Personalization personalization = new Personalization();
-        personalization.addTo(new Email(System.getenv("OPENHOUSE_EMAIL")));
-        
-        if(CONTACT_DIRECTOR.equals(category)) {
-            personalization.addCc(new Email(System.getenv("DIRECTOR_EMAIL")));
-        }
+	public Mail getContactMailFromRequest(String firstName, String lastName, String email, String phoneNumber,
+			String category, String message) throws IllegalFormatException {
+		final Mail mail = new Mail();
 
-        final Content content = new Content();
-        content.setType("text/html");
+		final Personalization personalization = new Personalization();
+		personalization.addTo(new Email(System.getenv("OPENHOUSE_EMAIL")));
 
-        final StringBuilder messageBody = new StringBuilder();
-        messageBody.append(String.format("<h3>%s - %s %s</h3><h5>%s", 
-                category, firstName, lastName, email));
-       
-        if(null != phoneNumber) {
-            messageBody.append(String.format(" - %s", phoneNumber));
-        }
-        
-        messageBody.append("</h5>");
-        messageBody.append("<p>" + message + "</p>");
+		if (CONTACT_DIRECTOR.equals(category)) {
+			personalization.addCc(new Email(System.getenv("DIRECTOR_EMAIL")));
+		}
 
-        content.setValue(messageBody.toString());
-        mail.addContent(content);
-        
-        mail.addPersonalization(personalization);
-        mail.setFrom(new Email(email));
-        mail.setSubject(String.format("%s - %s %s",         
-            category, firstName, lastName));     
+		final Content content = new Content();
+		content.setType("text/html");
 
-        return mail;
-    }
-    
-    public Mail getSignupMailFromRequest(
-    		String firstName,
-    		String lastName,
-    		String email,
-    		String dateText,
-    		String timeText) throws IllegalFormatException {
-        final Mail mail = new Mail();
-        
-        final Personalization personalization = new Personalization();
-        personalization.addTo(new Email(email));
-        
-        final Content content = new Content();
-        content.setType("text/html");
+		final StringBuilder messageBody = new StringBuilder();
+		messageBody.append(String.format("<h3>%s - %s %s</h3><h5>%s", category, firstName, lastName, email));
 
-        final StringBuilder messageBody = new StringBuilder();
-        messageBody.append(String.format("<h3>Audition Confirmation - %s %s</h3>", 
-                firstName, lastName));
-        
-        messageBody.append(String.format("<p> Dear %s, <br><br> This email is to confirm "
-        		+ "that you have scheduled an audition with the Open House Theatre Company on:"
-        		+ "<br><br><strong>%s at %s</strong><br><br> We Look forward to seeing you then!<br><br>"
-        		+ "<em>This email has been sent from a no-reply address, "
-        		+ "if you respond it will not be seen.</p>",
-        		firstName, dateText, timeText));
+		if (null != phoneNumber) {
+			messageBody.append(String.format(" - %s", phoneNumber));
+		}
 
-        content.setValue(messageBody.toString());
-        mail.addContent(content);
-        
-        mail.addPersonalization(personalization);
-        mail.setFrom(new Email("no-reply@openhousetheatre.com"));
-        mail.setSubject(String.format("Audition Confirmation - %s %s",         
-        	firstName, lastName));     
+		messageBody.append("</h5>");
+		messageBody.append("<p>" + message + "</p>");
 
-        return mail;
-    }
+		content.setValue(messageBody.toString());
+		mail.addContent(content);
 
-    public StaffMemberTO getStaffMemberTOFromRequest(final HttpServletRequest request) {
-    	final StaffMemberTO staffMember = new StaffMemberTO();
-    
-        if(StringUtils.isNotBlank(request.getParameter("firstName"))
-            && StringUtils.isNotBlank(request.getParameter("lastName"))
-            && StringUtils.isNotBlank(request.getParameter("title"))
-            && StringUtils.isNotBlank(request.getParameter("bio"))) {
-            
+		mail.addPersonalization(personalization);
+		mail.setFrom(new Email(email));
+		mail.setSubject(String.format("%s - %s %s", category, firstName, lastName));
+
+		return mail;
+	}
+
+	public Mail getSignupMailFromRequest(String firstName, String lastName, String email, String dateText,
+			String timeText) throws IllegalFormatException {
+		final Mail mail = new Mail();
+
+		final Personalization personalization = new Personalization();
+		personalization.addTo(new Email(email));
+
+		final Content content = new Content();
+		content.setType("text/html");
+
+		final StringBuilder messageBody = new StringBuilder();
+		messageBody.append(String.format("<h3>Audition Confirmation - %s %s</h3>", firstName, lastName));
+
+		messageBody.append(String.format("<p> Dear %s, <br><br> This email is to confirm "
+				+ "that you have scheduled an audition with the Open House Theatre Company on:"
+				+ "<br><br><strong>%s at %s</strong><br><br> We Look forward to seeing you then!<br><br>"
+				+ "<em>This email has been sent from a no-reply address, " + "if you respond it will not be seen.</p>",
+				firstName, dateText, timeText));
+
+		content.setValue(messageBody.toString());
+		mail.addContent(content);
+
+		mail.addPersonalization(personalization);
+		mail.setFrom(new Email("no-reply@openhousetheatre.com"));
+		mail.setSubject(String.format("Audition Confirmation - %s %s", firstName, lastName));
+
+		return mail;
+	}
+
+	public StaffMemberTO getStaffMemberTOFromRequest(final HttpServletRequest request) {
+		final StaffMemberTO staffMember = new StaffMemberTO();
+
+		if (StringUtils.isNotBlank(request.getParameter("firstName"))
+				&& StringUtils.isNotBlank(request.getParameter("lastName"))
+				&& StringUtils.isNotBlank(request.getParameter("title"))
+				&& StringUtils.isNotBlank(request.getParameter("bio"))) {
+
 			staffMember.setFirstName(request.getParameter("firstName"));
-			
-            staffMember.setLastName(request.getParameter("lastName"));
-            staffMember.setTitle(request.getParameter("title"));
-            staffMember.setBio(request.getParameter("bio"));
-        }
 
-        return staffMember;
-    }
+			staffMember.setLastName(request.getParameter("lastName"));
+			staffMember.setTitle(request.getParameter("title"));
+			staffMember.setBio(request.getParameter("bio"));
+		}
 
-    public String getStringFromRequest(HttpServletRequest request, String paramName) {
-        if (StringUtils.isNotBlank(request.getParameter(paramName))) {
-            return request.getParameter(paramName);
-        }
+		return staffMember;
+	}
 
-        return "";
-    }
+	public String getStringFromRequest(HttpServletRequest request, String paramName) {
+		if (StringUtils.isNotBlank(request.getParameter(paramName))) {
+			return request.getParameter(paramName);
+		}
 
-    public File getImageFromRequest(HttpServletRequest request) throws IOException, ServletException {
-        try {
-            Part filePart = request.getPart("picture");
-            InputStream fileContent = filePart.getInputStream();
-            File image = new File(Paths.get(filePart.getSubmittedFileName()).getFileName().toString());
-        
-            try (OutputStream outputStream = new FileOutputStream(image)) {
-                IOUtils.copy(fileContent, outputStream);
-            }
+		return "";
+	}
 
-            return image;
-        } catch (FileNotFoundException e) {
-            return null;
-        }
-    }
+	public File getImageFromRequest(HttpServletRequest request) throws IOException, ServletException {
+		try {
+			Part filePart = request.getPart("picture");
+			InputStream fileContent = filePart.getInputStream();
+			File image = new File(Paths.get(filePart.getSubmittedFileName()).getFileName().toString());
 
-    public Integer getIntFromRequest(HttpServletRequest request, String paramName) {
-        try {
-            return Integer.parseInt(request.getParameter(paramName));
-        } catch (NumberFormatException e) {
-            return 0;
-        }
-    }
+			try (OutputStream outputStream = new FileOutputStream(image)) {
+				IOUtils.copy(fileContent, outputStream);
+			}
 
-    @Deprecated
+			return image;
+		} catch (FileNotFoundException e) {
+			return null;
+		}
+	}
+
+	public Integer getIntFromRequest(HttpServletRequest request, String paramName) {
+		try {
+			return Integer.parseInt(request.getParameter(paramName));
+		} catch (NumberFormatException e) {
+			return 0;
+		}
+	}
+
+	@Deprecated
 	public PageSectionTO getPageSectionFromRequest(HttpServletRequest request) {
 		final PageSectionTO aboutSection = new PageSectionTO();
-		
+
 		if (StringUtils.isNotBlank(request.getParameter("content"))
 				&& StringUtils.isNotBlank(request.getParameter("sectionType"))) {
 			aboutSection.setSectionContent(request.getParameter("content"));
-			aboutSection.setSectionType(PageSectionType
-					.getTypeFromKey(request.getParameter("sectionType")));
+			aboutSection.setSectionType(PageSectionType.getTypeFromKey(request.getParameter("sectionType")));
 			aboutSection.setAdditionalProperties("");
 		}
 
-		
 		return aboutSection;
 	}
-	
+
 	public PageSectionTO getPageSectionFromRequest(String content, String sectionType) {
 		final PageSectionTO aboutSection = new PageSectionTO();
-		
-		if (StringUtils.isNotBlank(content)
-				&& StringUtils.isNotBlank(sectionType)) {
+
+		if (StringUtils.isNotBlank(content) && StringUtils.isNotBlank(sectionType)) {
 			aboutSection.setSectionContent(content);
-			aboutSection.setSectionType(PageSectionType
-					.getTypeFromKey(sectionType));
+			aboutSection.setSectionType(PageSectionType.getTypeFromKey(sectionType));
 			aboutSection.setAdditionalProperties("");
 		}
 
-		
 		return aboutSection;
 	}
-	
-	public SignupInformationTO getSignupInformationFromRequest(
-			String firstName,
-			String lastName,
-			String pronoun,
-			String dateText,
-			String timeText,
-			String email,
-			String phoneNumber) {
+
+	public SignupInformationTO getSignupInformationFromRequest(String firstName, String lastName, String pronoun,
+			String dateText, String timeText, String email, String phoneNumber) {
 		final SignupInformationTO signup = new SignupInformationTO();
-		
-		if(StringUtils.isNotBlank(firstName)
-				&& StringUtils.isNotBlank(lastName)
-				&& StringUtils.isNotBlank(pronoun)
-				&& StringUtils.isNotBlank(dateText)
-				&& StringUtils.isNotBlank(timeText)
-				&& StringUtils.isNotBlank(email)
+
+		if (StringUtils.isNotBlank(firstName) && StringUtils.isNotBlank(lastName) && StringUtils.isNotBlank(pronoun)
+				&& StringUtils.isNotBlank(dateText) && StringUtils.isNotBlank(timeText) && StringUtils.isNotBlank(email)
 				&& StringUtils.isNotBlank(phoneNumber)) {
 			signup.setFirstName(firstName);
 			signup.setLastName(lastName);
@@ -212,16 +184,38 @@ public final class ParameterService {
 			signup.setEmail(email);
 			signup.setPhoneNumber(phoneNumber);
 		}
-		
+
 		return signup;
 	}
 
+	public DateTO getDateFromRequest(String date, 
+			String location, 
+			String startTime, 
+			String endTime,
+			String information) {
+		final DateTO dateTo = new DateTO();
+
+		if (StringUtils.isNotBlank(date)
+				&& StringUtils.isNotBlank(startTime)
+				&& StringUtils.isNotBlank(endTime)
+				&& StringUtils.isNotBlank(location)
+				&& StringUtils.isNotBlank(information)) {
+			dateTo.setDate(date);
+			dateTo.setStartTime(startTime);
+			dateTo.setEndTime(endTime);
+			dateTo.setLocation(location);
+			dateTo.setInformation(information);
+		}
+
+		return dateTo;
+	}
+
+	@Deprecated
 	public DateTO getDateFromRequest(HttpServletRequest request) {
 		final DateTO date = new DateTO();
-		
-		if (StringUtils.isNotBlank(request.getParameter("date"))
-				&& StringUtils.isNotBlank(request.getParameter("time"))
-		    		&& StringUtils.isNotBlank(request.getParameter("endTime"))
+
+		if (StringUtils.isNotBlank(request.getParameter("date")) && StringUtils.isNotBlank(request.getParameter("time"))
+				&& StringUtils.isNotBlank(request.getParameter("endTime"))
 				&& StringUtils.isNotBlank(request.getParameter("location"))
 				&& StringUtils.isNotBlank(request.getParameter("information"))) {
 			date.setDate(request.getParameter("date"));
@@ -230,37 +224,32 @@ public final class ParameterService {
 			date.setLocation(request.getParameter("location"));
 			date.setInformation(request.getParameter("information"));
 		}
-		
+
 		return date;
 	}
 
-	public UserTO getUserFromRequest(String firstName, 
-			String lastName, 
-			String username, 
-			String hashedPassword) {
+	public UserTO getUserFromRequest(String firstName, String lastName, String username, String hashedPassword) {
 		final UserTO user = new UserTO();
-		
-		if(StringUtils.isNotBlank(firstName)
-				&& StringUtils.isNotBlank(lastName)
-				&& StringUtils.isNotBlank(username)
-				&& StringUtils.isNotBlank(hashedPassword)) {			
+
+		if (StringUtils.isNotBlank(firstName) && StringUtils.isNotBlank(lastName) && StringUtils.isNotBlank(username)
+				&& StringUtils.isNotBlank(hashedPassword)) {
 			user.setFirstName(firstName);
 			user.setLastName(lastName);
 			user.setUsername(username);
 			user.setPassword(hashedPassword);
 		}
-		
+
 		return user;
 	}
 
 	public UserTO getUserFromRequest(int id, String hashedPassword) {
 		final UserTO user = new UserTO();
-		
-		if(StringUtils.isNotBlank(hashedPassword)) {			
+
+		if (StringUtils.isNotBlank(hashedPassword)) {
 			user.setId(id);
 			user.setPassword(hashedPassword);
 		}
-		
+
 		return user;
 	}
 }
