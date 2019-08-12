8978a0582666723f3e87227bfcd909e1ceed95ab
Sam Lothamer <lothamer.sam@gmail.com>
2019-08-12 19:18:29 -0400

    add donate contorller

---
+++ src/main/java/com/openhouse/controllers/DonateController.java
@@ -0,0 +1,17 @@
+package com.openhouse.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DonateController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/pages/donate.jsp").forward(request, response);
+	}
+}
